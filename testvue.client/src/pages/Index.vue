25abd6547f94b0400f99a5dce3612f1e0f08937a
Siddhartha Sarkar <siddhartha.sarkar@lab49.com>
2026-01-01 13:00:45 -0600

    working vue ui

---
+++ testvue.client/src/pages/Index.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+  import { useAuth } from "@/auth";
+
+  const auth = useAuth();
+
+  async function login() {
+    await auth.msalInstance.initialize();
+    const loginResponse = await auth.msalInstance.loginPopup();
+    auth.account = loginResponse.account;
+
+    const response = await auth.msalInstance.acquireTokenSilent({
+      account: auth.account,
+      scopes: [`api://${import.meta.env.VITE_CLIENTID}/ourapi`]
+    });
+
+    auth.token = response.accessToken;
+  }
+
+</script> 
+
+<template>
+  <div>
+    <p>
+      <div v-if="auth.account">{{ auth.account.name }}</div>
+      <button v-if="!auth.account" @click="login" class="btn">Click here to login</button>
+    </p>
+  </div>
+</template>
